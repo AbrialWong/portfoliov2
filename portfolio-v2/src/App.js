84f3a0809a7b605f7522504d2a029117c7030a8e
abrial <abrial-wong@live.com.sg>
2022-10-18 14:22:09 +0800

    initial template for index page; edit css; clean codes

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,87 +1,170 @@
-import React from "react";
-import './App.css';
-import {Sidebar} from './Sidebar';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Sidebar } from "./Sidebar";
 
-import { makeStyles } from 'tss-react/mui';
+
+import {
+  Container,
+  Avatar,
+  Typography,
+  Button
+} from '@mui/material';
+
+//images, resume
+import profile from "./asserts/profile.jpeg";
+import resume from "./asserts/resume.pdf";
+
+//icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+import { makeStyles } from "tss-react/mui";
+// import { fontWeight } from "@mui/system";
+import { saveAs } from "file-saver";
+
 
 const useStyles = makeStyles()((theme) => {
   return {
     // Desktop Version
     // root
     root: {
+      backgroundColor: "#f4f4f4",
+      fontFamily: "Suez One",
     },
     // navbar
     navbar: {
-      backgroundColor: "lightpink",
-      display: "flex"
+      display: "flex",
     },
     navbarHam: {
-      flex:1,
+      flex: 1,
     },
     navbarLink: {
-      display:"flex",
-      marginRight:"30px"
+      display: "flex",
+      marginRight: "51px",
     },
     navbarTxt: {
-      padding: "5px",
-      margin: "5px",
-      color: "black",
-      fontWeight: "bold",
-      fontSize: "20px"
+      marginLeft: "5px",
+      fontSize: "20px",
+      marginRight: "36px",
+      marginBottom: "5px",
+      marginTop: "33px",
+      fontWeight:"bolder"
+
     },
     // content
     content: {
       // backgroundColor: "lightyellow"
     },
+    indexDiv:{
+      display: "flex",
+      marginTop: "189px",
+      paddingBottom: "135px"
+    },
+    indexImg:{
+      flex:1
+    },
+    indexDesc:{
+      flex:1,
+      marginTop: "70px"
+    },
+    indexDescTxt1:{
+      fontSize:"25px",
+      fontFamily: "Suez One",
+    },
+    indexDescTxt2:{
+      fontFamily: "Suez One",
+      fontSize:"52px",
+      color: "#c47f38",
+      fontWeight:"bolder"
+
+    },
+    indexDescTxt3:{
+      fontSize:"30px",
+      // marginTop:"49px"
+    },
+    indexDescSocial:{
+      color: "#c47f38",
+      marginTop:"39px"
+    },
+    indexDescBtn:{
+      backgroundColor: "#c47f38",
+      width: "182px",
+      height: "37px",
+      marginTop:"31px"
+    },
+
     // footer
     footer: {
-      backgroundColor: "palegreen",
-      textAlign: "center"
+      // backgroundColor: "palegreen",
+      textAlign: "center",
+      padding: "10px",
     },
-    
-
   };
 });
 
-
-
 export const App = () => {
-  const {classes} = useStyles(); // how to assign UseStyle
+  const { classes } = useStyles(); 
+
+  const saveFile = () => {
+    saveAs(
+      resume,
+      "Wong Yu Miao Abrial | Resume.pdf"
+    );
+  };
 
   return (
     <>
-      {/* navbar here */}
-      <div className={classes.navbar} id="outer-container">
-        <div className={classes.navbarHam}>
-          <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <div className={classes.root}>
+        {/* navbar here */}
+        <div className={classes.navbar} id="outer-container">
+          <div className={classes.navbarHam}>
+          {/* todo: To be edited later */}
+            {/* <Sidebar
+              pageWrapId={"page-wrap"}
+              outerContainerId={"outer-container"}
+            /> */}
+          </div>
+          <div className={classes.navbarLink}>
+            <div className={classes.navbarTxt}>About</div>
+            <div className={classes.navbarTxt}>Experience</div>
+            <div className={classes.navbarTxt}>Projects</div>
+            <div className={classes.navbarTxt}>Skills</div>
+            <div className={classes.navbarTxt}>Contact</div>
+          </div>
         </div>
-        <div className={classes.navbarLink}>
-          <div className={classes.navbarTxt}>
-            About     
-          </div>  
-          <div className={classes.navbarTxt}>
-            Experience   
-          </div>  
-          <div className={classes.navbarTxt}>
-            Projects    
-          </div>  
-          <div className={classes.navbarTxt}>
-            Skills  
-          </div>      
-          <div className={classes.navbarTxt}>
-            Contact  
-          </div> 
-        </div>
-      </div>
-      {/* content here */}
-      <div className={classes.content}>
-      content
-      </div>
-      {/* footer here */}
-      <div className={classes.footer}>
-        Proudly created by AbrialWong. Copyright @ 2022
-      </div>
+        {/* content here */}
+        <div className={classes.content}>
+          {/* index page */}
+          <div className={classes.indexDiv}>
+            <div className={classes.indexImg}>
+              <Avatar 
+                sx={{ width: 390, height: 391, border: "8px solid #ebded4",borderRadius: "50%", objectFit: "cover", marginLeft:"400px", 
+                }}
+                src={profile} />
+            </div>
+            <div className={classes.indexDesc}>
+              <Typography className={classes.indexDescTxt1}>Hello my name is </Typography>
+              <Typography className={classes.indexDescTxt2}>Abrial Wong </Typography>
+              <Typography className={classes.indexDescTxt3}>I am a software developer </Typography>
+              <div className={classes.indexDescSocial}>
+                <a href="https://github.com/AbrialWong"><GitHubIcon sx={{ width: 36, height: 36, color:"#c47f38"}}/></a>
+                <a href="https://www.linkedin.com/in/abrial-wong/"><LinkedInIcon sx={{ width: 36, height: 36, color:"#c47f38"}}/></a>
+              </div>
+              <Button variant="contained" className={classes.indexDescBtn} onClick={saveFile}>Resume </Button>
+            </div>
 
+          </div>
+          {/* <div style={{ border: "3px solid red" }}>About page</div>
+          <div style={{ border: "3px solid blue" }}>Experience page</div>
+          <div style={{ border: "3px solid green" }}>Projects page</div>
+          <div style={{ border: "3px solid purple" }}>Skills page</div>
+          <div style={{ border: "3px solid brown" }}>Contact page</div> */}
+        </div>
+        {/* footer here */}
+        {/* <div className={classes.footer}>
+          Proudly created by AbrialWong. Copyright @ 2022
+        </div> */}
+      </div>
     </>
   );
 };
--- portfolio-v2/src/Sidebar.js
+++ portfolio-v2/src/Sidebar.js
@@ -1,6 +1,94 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import './Sidebar.css';
+// import './Sidebar.css';
+import { makeStyles } from "tss-react/mui";
+
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    // Desktop Version
+    /* Position and sizing of burger button */
+bmBurgerButton: {
+  position: "fixed",
+  width: "36px",
+  height: "20px",
+  left: "36px",
+  top: "20px"
+},
+
+/* Color/shape of burger icon bars */
+bmBurgerBars: {
+  /* background: #373a47; */
+  background: "#c47f38"
+
+}
+
+// /* Color/shape of burger icon bars on hover*/
+// .bm-burger-bars-hover {
+//   /* background: #a90000; */
+//   background: #373a47;
+
+// }
+
+// /* Position and sizing of clickable cross button */
+// .bm-cross-button {
+//   height: 24px;
+//   width: 24px;
+// }
+
+// /* Color/shape of close button cross */
+// .bm-cross {
+//   background: #bdc3c7;
+// }
+
+// /*
+// Sidebar wrapper styles
+// Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
+// */
+// .bm-menu-wrap {
+//   position: fixed;
+//   height: 100%;
+// }
+
+// /* General sidebar styles */
+// .bm-menu {
+//   background: #373a47;
+//   padding: 2.5em 1.5em 0;
+//   font-size: 1.15em;
+// }
+
+// /* Morph shape necessary with bubble or elastic */
+// .bm-morph-shape {
+//   fill: #373a47;
+// }
+
+// /* Wrapper for item list */
+// .bm-item-list {
+//   color: #b8b7ad;
+//   padding: 0.8em;
+// }
+
+// /* Individual item */
+// .bm-item {
+//   display: inline-block;
+
+//   color: #d1d1d1;
+//   margin-bottom: 10px;
+//   text-align: left;
+//   text-decoration: none;
+//   transition: color 0.2s;
+// }
+
+// .bm-item:hover {
+//   color: #ffffff;
+// }
+
+// /* Styling of overlay */
+// .bm-overlay {
+//   background: rgba(0, 0, 0, 0.3);
+// }
+  };
+});
 
 
 export const Sidebar = () => {
